0484cd7453b0172eebb678892c317f4d6070c12a
AniltonFMoraisJr <anilton.morais@bovexo.com>
2024-04-02 14:12:08 -0300

    feat: Finishing sign up page layout

--- src/pages/auth/signIn.tsx
+++ src/pages/auth/signIn.tsx
@@ -2,6 +2,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import React from 'react'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import * as z from 'zod'
 
@@ -40,6 +41,9 @@ const SignIn: React.FC = () => {
     <>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button variant={'ghost'} asChild className="absolute right-8 top-8">
+          <Link to="/sign-up">Novo estabelicimento</Link>
+        </Button>
         <div className="flex w-[358px] flex-col justify-center gap-4">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
--- src/pages/auth/signUp.tsx
+++ src/pages/auth/signUp.tsx
@@ -2,6 +2,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import React from 'react'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { Link, useNavigate } from 'react-router-dom'
 import { toast } from 'sonner'
 import * as z from 'zod'
 
@@ -12,12 +13,17 @@ import { Label } from '@/components/ui/label'
 // import { Container } from './styles';
 
 const signUpForm = z.object({
+  restaurantName: z.string(),
+  managerName: z.string(),
+  phone: z.string(),
   email: z.string().email(),
 })
 
 type SignUpForm = z.infer<typeof signUpForm>
 
 const SignUp: React.FC = () => {
+  const navigate = useNavigate()
+
   const {
     register,
     handleSubmit,
@@ -29,10 +35,10 @@ const SignUp: React.FC = () => {
   async function handleSignUp(data: SignUpForm) {
     console.log(data)
     await new Promise((resolve) => setTimeout(resolve, 2000))
-    toast.success('Enviamos um link de autenticação para seu e-mail', {
+    toast.success('Restaurante cadastrado com sucesso!', {
       action: {
-        label: 'Reenviar',
-        onClick: () => handleSignUp(data),
+        label: 'Login',
+        onClick: () => navigate('/sign-in'),
       },
     })
   }
@@ -40,6 +46,9 @@ const SignUp: React.FC = () => {
     <>
       <Helmet title="Cadastro" />
       <div className="p-8">
+        <Button variant={'ghost'} asChild className="absolute right-8 top-8">
+          <Link to="/sign-in">Fazer login</Link>
+        </Button>
         <div className="flex w-[358px] flex-col justify-center gap-4">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
@@ -52,12 +61,36 @@ const SignUp: React.FC = () => {
 
           <form className="space-y-4" onSubmit={handleSubmit(handleSignUp)}>
             <div className="space-y-2">
+              <Label htmlFor="restaurantName">Nome do estabelimento</Label>
+              <Input id="restaurantName" {...register('restaurantName')} />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="managerName">Seu nome</Label>
+              <Input id="managerName" {...register('managerName')} />
+            </div>
+            <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
               <Input id="email" type="email" {...register('email')} />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="phone">Seu celular</Label>
+              <Input id="phone" type="tel" {...register('phone')} />
             </div>
             <Button className="w-full" type="submit" disabled={isSubmitting}>
               Finalizar cadastro
             </Button>
+
+            <p className="px-6 text-center text-sm leading-relaxed text-muted-foreground">
+              Ao continuar, você concorda com nossos{' '}
+              <a className="underline underline-offset-4" href="">
+                termos de serviço
+              </a>{' '}
+              e{' '}
+              <a className="underline underline-offset-4" href="">
+                políticas de privacidade
+              </a>
+              .
+            </p>
           </form>
         </div>
       </div>
