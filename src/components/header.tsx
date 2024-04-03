a12ba5ea1217631649c21ec094ac4d0f86d2136e
AniltonFMoraisJr <anilton.morais@bovexo.com>
2024-04-03 15:36:51 -0300

    fix: fixing style header; feat: implementing new page: Orders

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,7 +10,7 @@ import { Separator } from './ui/separator'
 
 const Header: React.FC = () => {
   return (
-    <div className="border-b">
+    <div className="flex border-b">
       <div className="flex h-16 items-center gap-6 px-6">
         <Pizza className="h-6 w-6" />
         <Separator orientation="vertical" className="h-6" />
