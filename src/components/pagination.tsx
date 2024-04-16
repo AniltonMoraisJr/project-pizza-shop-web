a57184c3f219b8757145b4edc1e2f8314e128797
AniltonFMoraisJr <anilton.morais@bovexo.com>
2024-04-16 13:46:30 -0300

    feat: Implementing pagination

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -14,12 +14,14 @@ export interface PaginationProps {
   pageIndex: number
   totalCount: number
   perPage: number
+  onPageChange: (pageIndex: number) => Promise<void> | void
 }
 
 const Pagination: React.FC<PaginationProps> = ({
   pageIndex,
   totalCount,
   perPage,
+  onPageChange,
 }) => {
   const pages = Math.ceil(totalCount / perPage) || 1
   return (
@@ -32,19 +34,39 @@ const Pagination: React.FC<PaginationProps> = ({
           Página {pageIndex + 1} de {pages}
         </div>
         <div className="flex items-center gap-3">
-          <Button variant={'outline'} className="h-8 w-8 p-0">
+          <Button
+            onClick={() => onPageChange(0)}
+            variant={'outline'}
+            className="h-8 w-8 p-0"
+            disabled={pageIndex === 0}
+          >
             <ChevronsLeft className="h-4 w-4" />
             <span className="sr-only">Primeira página</span>
           </Button>
-          <Button variant={'outline'} className="h-8 w-8 p-0">
+          <Button
+            onClick={() => onPageChange(pageIndex - 1)}
+            variant={'outline'}
+            className="h-8 w-8 p-0"
+            disabled={pageIndex === 0}
+          >
             <ChevronLeft className="h-4 w-4" />
             <span className="sr-only">Página anterior</span>
           </Button>
-          <Button variant={'outline'} className="h-8 w-8 p-0">
+          <Button
+            onClick={() => onPageChange(pageIndex + 1)}
+            variant={'outline'}
+            className="h-8 w-8 p-0"
+            disabled={totalCount <= pageIndex + 1}
+          >
             <ChevronRight className="h-4 w-4" />
             <span className="sr-only">Próxima página</span>
           </Button>
-          <Button variant={'outline'} className="h-8 w-8 p-0">
+          <Button
+            onClick={() => onPageChange(totalCount - 1)}
+            variant={'outline'}
+            className="h-8 w-8 p-0"
+            disabled={totalCount <= pageIndex + 1}
+          >
             <ChevronsRight className="h-4 w-4" />
             <span className="sr-only">Última página</span>
           </Button>
--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,6 +1,8 @@
 import { useQuery } from '@tanstack/react-query'
 import React from 'react'
 import { Helmet } from 'react-helmet-async'
+import { useSearchParams } from 'react-router-dom'
+import { z } from 'zod'
 
 import { getOrders } from '@/api/get-orders'
 import Pagination from '@/components/pagination'
@@ -18,10 +20,26 @@ import OrderTableRow from './order-table-row'
 // import { Container } from './styles';
 
 const Orders: React.FC = () => {
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const pageIndex = z.coerce
+    .number()
+    .transform((page) => page - 1)
+    .parse(searchParams.get('page') ?? '1')
+
   const { data: result } = useQuery({
-    queryKey: ['orders'],
-    queryFn: getOrders,
+    queryKey: ['orders', pageIndex],
+    queryFn: () => getOrders({ pageIndex }),
   })
+
+  function handlePaginate(pageIndex: number) {
+    setSearchParams((prev) => {
+      prev.set('page', (pageIndex + 1).toString())
+
+      return prev
+    })
+  }
+
   return (
     <>
       <Helmet title="Pedidos" />
@@ -52,7 +70,14 @@ const Orders: React.FC = () => {
               </TableBody>
             </Table>
           </div>
-          <Pagination pageIndex={0} totalCount={105} perPage={10} />
+          {result && (
+            <Pagination
+              pageIndex={result?.meta.pageIndex}
+              totalCount={result.meta.totalCount}
+              perPage={result.meta.perPage}
+              onPageChange={handlePaginate}
+            />
+          )}
         </div>
       </div>
     </>
