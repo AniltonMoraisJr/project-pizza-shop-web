d928580f7c20f867f22ff05c806c853b6d8fe7ee
AniltonFMoraisJr <anilton.morais@bovexo.com>
2024-04-19 13:13:53 -0300

    feat: Implementing interceptor to handle UNATHORIZED return from api

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { isAxiosError } from 'axios'
+import React, { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 import Header from '@/components/header'
+import { api } from '@/lib/axios'
 
 // import { Container } from './styles';
 
 const AppLayout: React.FC = () => {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    // Creating a listener for axios response events
+    const interceptorId = api.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (isAxiosError(error)) {
+          const status = error.response?.status
+          const code = error.response?.data.code
+
+          if (status === 401 && code === 'UNAUTHORIZED') {
+            navigate('/sign-in', { replace: true })
+          }
+        }
+      },
+    )
+
+    return () => {
+      api.interceptors.response.eject(interceptorId)
+    }
+  }, [navigate])
   return (
     <div className="flex min-h-screen flex-col antialiased">
       <Header />
